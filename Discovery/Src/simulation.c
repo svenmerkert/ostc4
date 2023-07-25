svenmerkert/ostc4

commit 04b98a23ee2bc499b5b7960ad894f7e5a785054c

Quit simualted dives after the dive time set in the SIM tab.

Discovery/Src/simulation.c
@@ -48,6 +48,7 @@
 
 //Private state variables
 static float sim_aim_depth_meter;
+static float sim_aim_time_minutes;
 static _Bool sim_heed_decostops = 1;
 
 static const float sim_descent_rate_meter_per_min = 20;
@@ -80,7 +81,7 @@ void simulation_set_heed_decostops(_Bool heed_decostops_while_ascending)
   ******************************************************************************
   * @return void
   */
-void simulation_start(int aim_depth)
+void simulation_start(int aim_depth, uint16_t aim_time_minutes)
 {
 	copyDiveSettingsToSim();
     copyVpmRepetetiveDataToSim();
@@ -90,6 +91,7 @@ void simulation_start(int aim_depth)
     if(aim_depth <= 0)
         aim_depth = 20;
     simulation_set_aim_depth(aim_depth);
+    sim_aim_time_minutes = aim_time_minutes;
     timer_init();
     set_stateUsedToSim();
     stateSim.lifeData.boolResetAverageDepth = 1;
@@ -130,6 +132,10 @@ void simulation_UpdateLifeData( _Bool checkOncePerSecond)
     static int last_second = -1;
     static _Bool two_second = 0;
     static float lastPressure_bar = 0;
+
+    if (sim_aim_time_minutes * 60 <= pDiveState->lifeData.dive_time_seconds) {
+        simulation_set_aim_depth(0);
+    }
 
     float localCalibCoeff[3] = { 0.0, 0.0, 0.0 };
     uint8_t index, index2;
@@ -912,6 +918,7 @@ void Sim_Quit (void)
         }
     }
 }
+
 void Sim_IncreasePPO(uint8_t sensorIdx)
 {
 	if((sensorIdx < NUM_OF_SENSORS) && (simSensmVOffset[sensorIdx] + SIM_PPO2_STEP < 100.0) && ((stateUsed->diveSettings.ppo2sensors_deactivated & (1 << sensorIdx)) == 0))

Discovery/Inc/simulation.h
@@ -42,7 +42,7 @@ typedef struct
     float	ppO2AtBottom;
 } SSimDataSummary;
 
-void simulation_start(int aim_depth);
+void simulation_start(int aim_depth, uint16_t aim_time_minutes);
 void simulation_exit(void);
 
 void simulation_set_heed_decostops(_Bool heed_decostops_while_ascending);

Discovery/Src/tMenuEditPlanner.c
@@ -86,7 +86,7 @@ void openEdit_Planner(uint8_t line)
             settingsGetPointer()->bluetoothActive = 0;
             MX_Bluetooth_PowerOff();
         }
-        simulation_start(tMplan_depth_meter);
+        simulation_start(tMplan_depth_meter, tMplan_dive_time_minutes);
         exitMenuEdit_to_Home();
         break;
     case 2:

commit 1ebcd036b6db85a1fe9a8ff14ec91cebb52e0be0

Fix bug introduced in 04b98a2, causing the deco planner to not show deco stops.

Discovery/Src/simulation.c
@@ -133,7 +133,7 @@ void simulation_UpdateLifeData( _Bool checkOncePerSecond)
     static _Bool two_second = 0;
     static float lastPressure_bar = 0;
 
-    if (sim_aim_time_minutes * 60 <= pDiveState->lifeData.dive_time_seconds) {
+    if (sim_aim_time_minutes && sim_aim_time_minutes * 60 <= pDiveState->lifeData.dive_time_seconds) {
         simulation_set_aim_depth(0);
     }
 
@@ -429,6 +429,7 @@ SDecoinfo* simulation_decoplaner(uint16_t depth_meter, uint16_t intervall_time_m
 
     //Going down / descent
     simulation_set_aim_depth(depth_meter);
+    sim_aim_time_minutes = 0;
     for(int i = 0; i < 60 * dive_time_minutes; i++)
     {
         simulation_UpdateLifeData(0);